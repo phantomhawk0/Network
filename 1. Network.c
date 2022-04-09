2c68a453606d437404193268ad02ac7278424d4e
phantomhawk0 <phantomhawk1@protonmail.com>
2022-04-08 22:38:28 -0400

    Types of Network
    Types of Network

---
+++ 1. Network.c
@@ -0,0 +1,195 @@
+1. NETWORK DEFINITION
+
+		
+		- A group or system of interconnected people or things.” Similarly, in the computer world, the term network means two or more connected computers that can 
+		  share resources such as data and applications, office machines, an Internet connection, or some combination of these.
+
+		  These two hosts “talk” to each other using a computer language called binary code.
+
+
+
+
+2. LOCAL AREA NETWORK (LAN)
+
+
+		- Local area network (LAN) is usually restricted to spanning a particular geographic location such as an office building, a single department within a 
+		  corporate office, or even a home office. Back in the day, you couldn’t put more than 30 workstations on a LAN. ven so, it’s still best to split a
+		  big LAN into smaller logical zones known as workgroups to make administration easier.
+
+
+
+
+
+3. COMMON NETWORK COMPONENTS
+
+	
+		1. Workstation
+		2. Server 
+		3. Hosts  
+
+
+
+		- WORKSTATION: Workstations are often seriously powerful computers that run more than one central processing unit (CPU) and whose resources are available to 
+		  other users on the network to access when needed.
+
+
+		- SERVER: Servers are also powerful computers. They get their name because they truly are “at the service” of the network and run specialized software 
+		  known as the network operating system to maintain and control the network.
+
+			- File Server: Stores and dispenses files
+			- Mail Server The network’s post office; handles email functions
+			- Print Server Manages printers on the network
+			- Web Server Manages web-based activities by running Hypertext Transfer Protocol Secure (HTTPS) for storing web content and accessing web pages
+			- Fax Server The “memo maker” that sends and receives paperless faxes over the network
+			- Application Server: Manages network applications
+			- Telephony Server Handles the call center and call routing and can be thought of as a sophisticated network answering machine
+			- Proxy Server Handles tasks in the place of other machines on the network, particularly an Internet connection
+
+
+		- HOSTS: Host means any network device with an IP address
+
+
+
+
+
+4. METROPOLITAN AREA NETWORK (MAN) 
+
+		- Network covering a metropolitan area is used to interconnect various buildings and facilities usually over a carrier-provided network. 
+
+
+
+
+5. WIDE AREA NETWORK (WAN)
+
+
+		- WAN networks are what we use to span large geographic areas and truly go the distance. Like the Internet, WANs usually employ both routers and public links
+
+
+
+
+
+6. PERSONAL AREA NETWORK (PAN)
+
+
+		- For close proximity connections there are PANs or personal area networks. These are seen with smartphones and laptops in a conference room where local 
+		  connections are used to collaborate and send data between devices. While a PAN can use a wired connection such as Ethernet or USB, it is more common that 
+		  short distance wireless connections are used such as Bluetooth, infrared, or ZigBee.
+
+
+
+
+
+7. CAMPUS AREA NETWORK (CAN) 
+
+
+		- A CAN, or campus area network, covers a limited geographical network such as a college or corporate campus. The CAN typically interconnect LANs in various 
+		  buildings and offers a Wi-Fi component for roaming users.
+
+
+
+
+
+8. STORAGE AREA NETWORK (SAN)
+
+
+		- A storage area network (SAN) is designed for and used exclusively by storage systems. SANs interconnect servers to storage arrays containing 
+		  centralized banks of hard drive or similar storage media. 
+
+
+
+
+
+9. SOFTWARE-DEFINED WIDE AREA NETWORK (SDWAN)
+
+
+		- A software-defined wide area network (SDWAN) is a virtual WAN architecture that uses software to manage connectivity, devices, and 
+		  services and can make changes in the network based on current operations.
+		  SDWANs integrate any type of transport architectures such as MPLS, LTE, and broadband Internet services to securely connect users to applications.
+
+
+
+
+
+10. MULTIPROTOCOL LABEL SWITCHING (MPLS) is
+
+
+		- MPLS is a switching mechanism that imposes labels (numbers) to data and then uses those labels to forward data when it arrives at the MPLS network
+
+
+
+
+11. MULTIPOINT GENERIC ROUTING ENCAPSULATION (mGRE)
+
+
+		- The Multipoint Generic Routing Encapsulation (mGRE) protocol refers to a carrier or service provider offering that dynamically creates and terminates 
+		  connections to nodes on a network. mGRE is used in Dynamic Multipoint VPN deployments. The protocol enables dynamic connections without having to 
+		  pre-configure static tunnel endpoints.
+		  The protocol encapsulates user data, creates a VPN connection to one or many nodes and when completed, tears down the connection.
+
+
+
+
+12. PEER-TO-PEER NETWORK 
+
+
+		- Computers connected together in peer-to-peer networks do not have any central, or special, authority—they’re all peers, meaning that when it 
+		  comes to authority, they’re all equals.
+
+
+
+
+
+13. CLIENT-SERVER NETWORK 
+
+
+		- Client-Server networks are pretty much the polar opposite of peer-to-peer networks because,in them, a single server uses a network operating system 
+		  for managing the whole network.
+
+
+
+
+14. PHYSICAL NETWORK TOPOLOGIES nodes
+
+
+		1. Bus
+		2. Star/hubs and spoke
+		3. Ring 
+		4. Mesh 
+		5. Point-to-point
+		6. Point-to-multipoint
+		7. Hybrid
+
+
+
+		1. BUS TOPOLOGY: Bus topology is a specific kind of network topology in which all of the various devices in the network are connected to a single cable or line. 
+						 In general, the term refers to how various devices are set up in a network.his type of topology is the most basic one of the bunch. 
+						 The bus topology consists of two distinct and terminated ends, with each of its computers connecting to one unbroken cable running its 
+						 entire length. 
+
+				- Advantage It is easy to install and is not expensive.
+				- Disadvantage: Difficult to troubleshoot, change or move
+
+
+		2. START/HUBS TOPOLOGY: A star (hub-and-poke) topology’s computers are connected to a central point with their own individual cables or wireless connections. 
+		                        You’ll often find that central spot inhabited by a device like a hub, a switch, or an access point.
+
+		        - Advantage: Star topology offers a lot of advantages over bus topology, making it more widely used even though it obviously requires more physical 
+		                     media. One of its best features is that because each computer or network segment is connected to the central device individually, if
+							 the cable fails, it only brings down the machine or network segment related to the point of failure. This makes the network much more 
+							 fault-tolerant as well as a lot easier to troubleshoot. Another great thing about a star topology is that it’s a lot more scalable—all 
+							 what you have to do if you want to add to it is run a new cable and connect to the machine at the core of the star.extension
+
+
+
+					- Version: Point-to-point link and wireless
+
+
+
+
+
+		3. RING TOPOLOGIES
+
+				- In this type of topology, each computer is directly connected to other computers within the same network.
+
+
+
